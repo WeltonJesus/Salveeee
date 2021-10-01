e7d18a2745003965a8ae89f128297f4e3ff64ea7
Welton <welton-ap@hotmail.com>
2021-10-01 17:57:49 -0300

    First

---
+++ src/entities/Category.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Category {
+
+    private Integer id;
+    private String name;
+    private Integer tier;
+
+    public Category() {
+    }
+
+    public Category(Integer id, String name, Integer tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
+
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
+}
